571646412233117686fa00a4650c584b1742786a
Ramirez <tomas.ramirez.ext@wts.com>
2023-05-31 16:12:14 -0600

    Added Ec2Rdp tool. Currently, it takes two parameters, the instance name, and the RDP file path. It starts the instance if it's not running (including waiting for it to transition between states if it's not stopped). It then finds the public IP and copies it to the RDP file.
    It depends on the default profile. I'm still pretty new to AWS programming; so I'll say I set my default profile in ~/.aws/credentials.
    
    Also, my instances don't use key pairs, and I don't yet know how those work; so I don't know if this will work with those.

---
+++ Ec2Tools-sln/Ec2Rdp/Program.cs
@@ -0,0 +1,173 @@
+﻿using Amazon.EC2;
+using Amazon.EC2.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ec2Rdp
+{
+    internal class Program
+    {
+        static void logMsg(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static void logWarn(string message)
+        {
+            Console.WriteLine($"WARN: {message}");
+        }
+
+        static void logError(string message)
+        {
+            Console.WriteLine($"ERROR: {message}");
+        }
+
+        static void promptExit(int exitCode)
+        {
+            Console.WriteLine("\nPress enter to exit.");
+            Console.ReadLine();
+            Environment.Exit(exitCode);
+        }
+
+        static Instance getInstance(AmazonEC2Client ec2Client, string instanceName)
+        {
+            var result = ec2Client.DescribeInstances(new DescribeInstancesRequest
+            {
+                Filters = new List<Filter> {
+                    new Filter {
+                        Name = "tag:Name",
+                        Values = new List<string> {
+                            instanceName
+                        }
+                    }
+                }
+            });
+
+            if (result.Reservations.Count == 0)
+            {
+                logError("There are no matching reservations");
+                promptExit(1);
+            }
+            if (result.Reservations.Count > 1)
+            {
+                logWarn($"There's more than 1 matching reservation. Count = {result.Reservations.Count}");
+            }
+
+            Instance instance;
+            {
+                var reservation = result.Reservations[0];
+                if (reservation.Instances.Count == 0)
+                {
+                    logError("There are no matching instances");
+                    promptExit(1);
+                }
+                instance = reservation.Instances[0];
+            }
+
+            return instance;
+        }
+
+        static async Task Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                throw new ArgumentException("Expected >=2 arguments: instanceName rdpFilePath [startInstance]");
+            }
+
+            string instanceName = args[0];
+            logMsg($"instanceName: {instanceName}");
+            string rdpFilePath = args[1];
+            logMsg($"rdpFilePath: {rdpFilePath}");
+
+            var ec2Client = new AmazonEC2Client();
+            var instance = getInstance(ec2Client, instanceName);
+
+            if (instance.State.Name == InstanceStateName.Terminated)
+            {
+                logError("The instance is terminated");
+                promptExit(1);
+            }
+            if (instance.State.Name == InstanceStateName.ShuttingDown)
+            {
+                logError("The instance is shutting down (terminating)");
+                promptExit(1);
+            }
+
+            const int SLEEP_MS = 3000;
+            const int MAX_WAIT_MS = 60000;
+            const int MAX_LOOP = MAX_WAIT_MS / SLEEP_MS;
+            var loopInd = 0;
+            if (instance.State.Name == InstanceStateName.Stopping)
+            {
+                while (instance.State.Name == InstanceStateName.Stopping && loopInd++ < MAX_LOOP)
+                {
+                    logMsg("Instance is stopping. Waiting for it to finish before starting again...");
+                    Thread.Sleep(SLEEP_MS);
+                    instance = getInstance(ec2Client, instanceName);
+                }
+                if (instance.State.Name != InstanceStateName.Stopped)
+                {
+                    logError($"Instance did not stop. Current state is {instance.State.Name}. Try again later.");
+                    promptExit(1);
+                }
+            }
+
+            if (instance.State.Name == InstanceStateName.Stopped)
+            {
+                logMsg("Instance is stopped. Starting it...");
+                ec2Client.StartInstances(new StartInstancesRequest(new List<string> { instance.InstanceId }));
+                Thread.Sleep(SLEEP_MS);
+                instance = getInstance(ec2Client, instanceName);
+            }
+
+            loopInd = 0;
+            if (instance.State.Name == InstanceStateName.Pending)
+            {
+                while (instance.State.Name == InstanceStateName.Pending && loopInd++ < MAX_LOOP)
+                {
+                    logMsg("Instance is pending. Waiting for it to become running...");
+                    Thread.Sleep(SLEEP_MS);
+                    instance = getInstance(ec2Client, instanceName);
+                }
+                if (instance.State.Name != InstanceStateName.Running)
+                {
+                    logError($"Instance state did not become running. Current state is {instance.State.Name}. Try again later.");
+                    promptExit(1);
+                }
+            }
+
+            // I think this case usually shouldn't happen unless the instance takes extra long to start, or there's a bug in the logic above
+            if (instance.State.Name != InstanceStateName.Running)
+            {
+                logError($"Instance is not running. Current state is {instance.State.Name}");
+                promptExit(1);
+            }
+
+            var publicIp = instance.PublicIpAddress;
+            if (string.IsNullOrWhiteSpace(publicIp))
+            {
+                logError("IP is empty");
+                promptExit(1);
+            }
+            logMsg($"Public IP: {publicIp}");
+
+            // update the IP address in the RDP file
+            string rdpTextOrig = File.ReadAllText(rdpFilePath);
+            string matchVal = Regex.Match(rdpTextOrig, "full address:s:.*").Value;
+            var rdpTextUpdated = Regex.Replace(rdpTextOrig, "full address:s:.*", $"full address:s:{publicIp}");
+            File.WriteAllText(rdpFilePath, rdpTextUpdated);
+
+
+            // launch RDP session
+            Console.WriteLine("Launching RDP session...");
+            Process.Start("mstsc", $"\"{rdpFilePath}\"");
+        }
+    }
+}
---
+++ Ec2Tools-sln/Ec2Rdp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ec2Rdp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Suez")]
+[assembly: AssemblyProduct("Ec2Rdp")]
+[assembly: AssemblyCopyright("Copyright © Suez 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("15c4b103-0b85-4cad-847c-df81490d23fe")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
